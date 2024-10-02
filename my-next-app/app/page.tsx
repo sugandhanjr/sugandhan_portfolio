40b0e115c46ddf46fe32ae57c0ad99a7f7add630
sugandhanjr <sudandhanjr@gmail.com>
2024-10-02 19:16:39 +0530

    15

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             Wellcome To My Portfolio
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              SUGANDHANJR
+              SUGANDHAN
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block">
---
+++ my-next-app/components/Navigation.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+
+const Navigation = () => {
+    const [isRouting, setisRouting] = useStste(flase)
+  return (
+    <div>Navigation</div>
+  )
+}
+
+export default Navigation
