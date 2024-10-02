ede4655bf885c3625f5c866ce1ecbbb0b601a353
sugandhanjr <sudandhanjr@gmail.com>
2024-10-02 19:44:09 +0530

    error fixed

--- my-next-app/components/Navigation.tsx
+++ my-next-app/components/Navigation.tsx
@@ -1,11 +1,33 @@
 "use client"
 
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
 
 const Navigation = () => {
-    const [isRouting, setisRouting] = useStste(flase)
+    const [isRouting, setisRouting] = useState(false)
+    const path = usePathname()
+    const[prevPath, setPrevPath] =useState("/")
+
+    useEffect(() => {
+
+        if(prevPath !== path) {
+            setisRouting(true)
+        }
+    },[path,prevPath])
+
+    useEffect(() => {
+        if(isRouting){
+            setPrevPath(path);
+            const timeout = setTimeout(() => {
+                setisRouting(false)
+            },1200)
+            return() => clearTimeout(timeout)
+        }
+    },[isRouting])
+    
   return (
-    <div>Navigation</div>
+    <div style={{ left:"20px"}}
+    className='absolute z-[50] -bottom-20 w-[50%] md:w[20%] max-h-[150px] rounded-full flex justify-between items-center border border-white'></div>
   )
 }
 
