116fce5ba485f2d7e820f99dd8acca619847f9cb
sugandhanjr <sudandhanjr@gmail.com>
2024-10-02 14:34:57 +0530

    error fixed

--- my-next-app/app/layout.tsx
+++ my-next-app/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "webchain dev",
+  title: "sugandhanjr",
   description: "this is my portfolio",
 };
 
--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -7,18 +7,19 @@ export default function Home() {
       <div className="flex items-center w-full h-full bg-cover bg-center" style={{ backgroundImage: "url(/main-bg.webp)" }}>
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
-            Make anything possible with
+            Wellcome To My Portfolio
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              web development
+              {" "}
+              SUGANDHANJR
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block">
             lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
 
           </p>
-          <div className="flex-col md:flex-row hidden md:flex" gap-5>
+          <div className="flex-col md:flex-row hidden md:flex gap-5">
             <Link href="/my-skills" className="rounded-[20px]  group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
-              learn more
+              Learn More
             </Link>
             <Link href="/my-projects" className="rounded-[20px]  group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
               <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
@@ -35,7 +36,7 @@ export default function Home() {
 
         </div>
       </div>
-      <div className=" absolute bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
+      <div className=" absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
         <Link href="/my-skills" className="rounded-[20px]  group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
           learn more
         </Link>
